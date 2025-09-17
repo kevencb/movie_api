262d2be7cbb49affce2c34371e95a0c9f8f9ce81
Kev_CB <kevcb369@gmail.com>
2025-09-17 16:18:54 -0500

    Carga de películas populares y categorias de películas

--- src/main.js
+++ src/main.js
@@ -1,2 +1,69 @@
+const API_URL = "https://api.themoviedb.org/3/"
 
-console.log('API URL = https://luhiulhiusf.com/ahiuhiudaiu?api_jey=' + API_KEY)
+async function apiFetch(endpoint, method = "GET", body = null, language = "es-ES") {
+    const headers = {
+        "accept": "application/json;charset=utf-8",
+        "Authorization": API_KEY
+    }
+
+    const options = {
+        method,
+        headers
+    }
+
+    if (method !== "GET" && body) {
+        options.body = JSON.stringify(body)
+    }
+
+    let url = `${API_URL}${endpoint}${endpoint.includes('?') ? '&' : '?'}language=en-EN`
+    const response = await fetch(url, options)
+
+    if (!response.ok) {
+        const error = await response.json()
+        throw new Error(error.status_message || "Error en la solicitud.")
+    }
+
+    const data = response.json()
+    return data
+}
+
+// Carga de peliculas populares
+async function getTrendingMoviesPreview() {
+    const { results: movies } = await apiFetch("movie/popular")
+
+    const trendingPreview = document.querySelector("#trendingPreview .trendingPreview-movieList")
+    movies.forEach(movie => {
+        const movieContainer = document.createElement('div')
+        movieContainer.classList.add("movie-container")
+
+        const moviePoster = document.createElement('img')
+        moviePoster.classList.add('movie-img')
+        moviePoster.setAttribute('alt', movie.title)
+        moviePoster.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + movie.poster_path)
+
+        movieContainer.appendChild(moviePoster);
+        trendingPreview.appendChild(movieContainer)
+
+    });
+}
+
+// Carga de categorías
+async function getCategoriesPreview() {
+    const { genres: categories } = await apiFetch("genre/movie/list")
+    const categoriesPreview = document.querySelector("#categoriesPreview .categoriesPreview-list")
+    categories.forEach(category => {
+        const categoryContainer = document.createElement('div')
+        categoryContainer.classList.add('category-container')
+        const categoryTitle = document.createElement('h3')
+        categoryTitle.setAttribute("id", 'id' + category.id)
+        categoryTitle.classList.add("category-title")
+        categoryTitle.textContent = category.name
+
+        categoryContainer.appendChild(categoryTitle)
+        categoriesPreview.appendChild(categoryContainer)
+    })
+}
+
+
+getTrendingMoviesPreview()
+getCategoriesPreview()
